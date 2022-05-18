138c823fff9a9811149fc8807fca99171c23da71
summerGreen-11 <70504884+summerGreen-11@users.noreply.github.com>
2022-05-18 12:29:27 +0900

    mouse rotation
    0518) 마우스를 이용한 모델 회전 기능 추가

---
+++ 3D Model Opacity Test/Assets/Scenes/RotateAround.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public GameObject stage;
+
+    private float xRotateMove, yRotateMove;
+
+    public float rotateSpeed = 500.0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            xRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+            Vector3 stagePosition = stage.transform.position;
+
+            transform.RotateAround(stagePosition, Vector3.up, xRotateMove);
+
+            transform.LookAt(stagePosition);
+        }
+    }
+}
