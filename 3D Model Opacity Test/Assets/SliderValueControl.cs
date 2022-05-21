e214356eecb5b6986fdcc4afd0cd79c7e44a9e68
yeenii <jye0042@naver.com>
2022-05-21 22:23:19 +0900

    Camera commit
    정예은 Orthographic 수정 커밋

--- 3D Model Opacity Test/Assets/SliderValueControl.cs
+++ 3D Model Opacity Test/Assets/SliderValueControl.cs
@@ -14,7 +14,7 @@ public class SliderValueControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
@@ -24,6 +24,9 @@ public class SliderValueControl : MonoBehaviour
         float slvalue2 = sl2.value;
         float slvalue3 = sl3.value;
 
+  
+    
+        
         //알고리즘 고민하기
 
     }
