5dd672af69090cf16ff78db554dd97165bcda75c
summerGreen-11 <70504884+summerGreen-11@users.noreply.github.com>
2022-05-21 23:24:54 +0900

    slider setting algoritm
    0521) 그린 - 슬라이더 조정 알고리즘 업데이트

--- 3D Model Opacity Test/Assets/SliderValueControl.cs
+++ 3D Model Opacity Test/Assets/SliderValueControl.cs
@@ -10,16 +10,6 @@ public class SliderValueControl : MonoBehaviour
 	bool chk = false;
 	int temp;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-
-
-   
-
-	}
-
-
 	// Update is called once per frame
 	void Update()
 	{
